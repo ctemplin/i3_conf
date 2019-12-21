fcf127a966e0e8f4d9252a17aba569f88e08f799
Templin, Chris <ctemplin@gmail.com>
2019-12-21 11:05:54 -0500

    Add 1 second timeout to notifications.

--- .i3/disable-standby-fs.py
+++ .i3/disable-standby-fs.py
@@ -23,19 +23,19 @@ def on_fullscreen_mode(i3, e):
     if len(find_fullscreen(i3.get_tree())):
         # call(['xset', 's', 'off'])
         # call(['xset', '-dpms'])
-        call(['notify-send', 'Screen Lock Disabled'])
+        call(['notify-send', '-t', '1000', 'Screen Lock Disabled'])
         call(['xautolock', '-disable'])
     else:
         # call(['xset', 's', 'on'])
         # call(['xset', '+dpms'])
-        call(['notify-send', 'Screen Lock Enabled'])
+        call(['notify-send', '-t', '1000', 'Screen Lock Enabled'])
         call(['xautolock', '-enable'])
 
 def on_window_close(i3, e):
     if not len(find_fullscreen(i3.get_tree())):
         # call(['xset', 's', 'on'])
         # call(['xset', '+dpms'])
-        call(['notify-send', 'Screen Lock Enabled'])
+        call(['notify-send', '-t', '1000', 'Screen Lock Enabled'])
         call(['xautolock', '-enable'])
 
 i3.on('window::fullscreen_mode', on_fullscreen_mode)
