fad12e10b2e4280770272ebed73ef8967fa390bc
Templin, Chris <ctemplin@gmail.com>
2021-09-29 13:16:35 -0400

    Messaging about edge conditions. When there are no marks at all or only mark is already focused.

--- .i3/bin/i3-cycle-marks.sh
+++ .i3/bin/i3-cycle-marks.sh
@@ -8,24 +8,31 @@ declare -a "MARK_LIST=(`i3-msg -t get_marks | jq -rj '.[] | (. + " ")'`)"
 # Get the the first mark (if any) of the currently focused window.
 MARK_OF_FOCUSED=`i3-msg -t get_tree | jq -j ' recurse(.nodes[]) | select(.focused == true) | .marks[0] '`
 
+if [[ "${#MARK_LIST[@]}" == 0 ]]; then
+  notify-send "No marked windows"
+fi
+
 # Focused window is unmarked; target the first mark in the list.
 TARGET_MARK_INDEX=0
 
 # Focused winow is marked; target the next or prev mark.
-if [ "$MARK_OF_FOCUSED" != "null" ]
-then
-  # Find the array index of the current mark.
-  for i in "${!MARK_LIST[@]}"; do
-    if [[ "${MARK_LIST[$i]}" == "${MARK_OF_FOCUSED}" ]]; then
-      # Increment or decrement.
-      TARGET_MARK_INDEX=$(( i + INCVAL ));
-      # If beyond bound wrap around to beginning of array. No need to reverse
-      # wrap to end of array; an index of -1 is equivalent to array length - 1.
-      if (( $TARGET_MARK_INDEX >= ${#MARK_LIST[@]} )); then
-        TARGET_MARK_INDEX=0
+if [ "$MARK_OF_FOCUSED" != "null" ]; then
+  if [[ "${#MARK_LIST[@]}" == 1 ]]; then
+    notify-send "No other marked windows"
+  else
+    # Find the array index of the current mark.
+    for i in "${!MARK_LIST[@]}"; do
+      if [[ "${MARK_LIST[$i]}" == "${MARK_OF_FOCUSED}" ]]; then
+        # Increment or decrement.
+        TARGET_MARK_INDEX=$(( i + INCVAL ));
+        # If beyond bound wrap around to beginning of array. No need to reverse
+        # wrap to end of array; an index of -1 is equivalent to array length - 1.
+        if (( $TARGET_MARK_INDEX >= ${#MARK_LIST[@]} )); then
+          TARGET_MARK_INDEX=0
+        fi
       fi
-    fi
-  done
+    done
+  fi
 fi
 
 # notify-send -u low -t 1500 \
