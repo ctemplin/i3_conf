4e1cb58eb1faf02dc8d4276c0068fd10c6a460fb
Templin, Chris <ctemplin@gmail.com>
2021-10-03 08:41:32 -0400

    Format changes

--- .i3/bin/ws/14-kbd.sh
+++ .i3/bin/ws/14-kbd.sh
@@ -9,7 +9,7 @@ if test $CWS -ne $WS ; then i3-msg workspace number $WS; fi;
 # Only populate if current tree is empty (1 line)
 if test `i3-save-tree --workspace ${WS} | grep -c .` -lt 2 ; then 
   # TerminessTFF Nerd Font char UxF812 in string below
-  i3-msg rename workspace to "\"`echo -e ${WS}: kbd`\""
+  i3-msg rename workspace to "\"`echo -e ${WS}:kbd`\""
   # i3-msg rename workspace to "${WS}:kbd"
   i3-msg append_layout "~/.i3/ws/workspace-${WS}-kbd.jsonc"
   brave --new-window --app=https://configure.zsa.io/my_layouts &
