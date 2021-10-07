fcfbfa451dfae109cf01a60d5cbc4e8a0234e451
Templin, Chris <ctemplin@gmail.com>
2021-10-07 16:04:36 -0400

    Custom border toggle script. Skipping "none" border state when toggling. "none" bound to +Shift bindsym variation. Using B key for border. Moving back-and-forth to J for "jump".

---
+++ .i3/bin/i3-border-toggle-partial.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+# Toggle between 2 of the 3 border states (pixel and normal)
+# Will come out of 3rd state (none) but not re-enter
+
+i3-border-toggle-partial ()
+{
+  local -A NEXTBORDER=( [none]="normal" [pixel]="normal" [normal]="pixel" )
+  local CURBORDER=`i3-msg -t get_tree | jq -j ' recurse((.nodes, .floating_nodes)[]) | select(.focused == true) | .border'`
+  i3-msg border ${NEXTBORDER[${CURBORDER}]}
+}
+
+i3-border-toggle-partial
