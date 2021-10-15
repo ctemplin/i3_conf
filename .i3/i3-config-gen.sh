4b8e2b298de892935e581ad3eecb43b284bd095c
Templin, Chris <ctemplin@gmail.com>
2021-10-15 12:04:03 -0400

    Reworking config generation, toggling ws numbers. Moved ws defs back into config template. Simplifed defs. Number now inferred from list position. Adding script and key binds to toggle ws numbers.

--- .i3/i3-config-gen.sh
+++ .i3/i3-config-gen.sh
@@ -2,29 +2,6 @@
 
 i3-config-gen ()
 {
-  # "placeholder;layout-tag;number;glyph;label"
-  declare -a wsArr=(
-    "{{{ws1}}};pkg;1;;pkgs"
-    "{{{ws2}}};snd;2;;audio"
-    "{{{ws3}}};code;3;謹;code"
-    "{{{ws4}}};term;4;‎ﲵ;term"
-    "{{{ws5}}};___;5;;___"
-    "{{{ws6}}};bar;6;瞧;bar"
-    "{{{ws7}}};net;7;;net"
-    "{{{ws8}}};web;8;拓;web"
-    "{{{ws9}}};___;9;;___"
-    "{{{ws10}}};sec;10;聾;sec"
-    "{{{ws11}}};___;11;;___"
-    "{{{ws12}}};___;12;;___"
-    "{{{ws13}}};comm;13;‎שּ;comms"
-    "{{{ws14}}};kbd;14;;kbd"
-    "{{{ws15}}};ws;15;;ws"
-    "{{{ws16}}};ws;16;;ws"
-    "{{{ws17}}};ws;17;;ws"
-    "{{{ws18}}};ws;18;;ws"
-    "{{{ws19}}};ws;19;;ws"
-    "{{{ws20}}};ws;20;;ws"
-  )
 
   local configTmpl=~/.i3/config.tmpl
   local workFile=~/.i3/config-gen-output/config.temp
@@ -32,19 +9,45 @@ i3-config-gen ()
 
   /usr/bin/cp ${configTmpl} ${workFile}
 
-  # loop through the array of ws tokenized strings
-  for wsLine in ${wsArr[@]}; do
-    readarray -d ';' -t tokens<<< "$wsLine"
+  local wsNum
 
-    # format the tokens into the new workspace name
-    printf -v wsFormat "%s:%s%s" ${tokens[2]} ${tokens[3]} ${tokens[4]}
-    echo $wsFormat
+  declare -a wsKeys=(1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0)
+
+  # extract the work space key number (2 for each w/w\o Ctrl)
+  # Define the magic beginning and ending comments
+  local START_LINE_PRTL='# BEGIN WORKSPACE ORDER'
+  local END_LINE_PRTL='# END WORKSPACE ORDER'
+
+  # Get the line number of the beginning comment
+  read -d ':' lineNum <<< $(grep -no "${START_LINE_PRTL}" ${configTmpl})
+
+  # Read the line between into an array
+  declare -a wsArr=($(grep -Pzo "(?s)${START_LINE_PRTL}\N*.\K(.*)(?=${END_LINE_PRTL})" ${configTmpl}))
+
+  # Loop through the array by index
+  for i in ${!wsArr[@]}; do
+    local wsNum=$(( $i + 1 ))
+    local wsKey=${wsKeys[$i]}
+    if (( $i > 9 )); then wsKey="Ctrl+${wsKey}"; fi
+
+    # format the number and name into the new workspace name
+    printf -v wsFormat "%s:%s" $wsNum ${wsArr[$i]}
+    printf -v bindSyms \
+    "bindsym \$mod+%s workspace \\\"%s\\\";\\\n\
+    bindsym \$mod+Shift+%s move container to workspace \\\"%s\\\"; workspace \\\"%s\\\";"\
+    $wsKey $wsFormat $wsKey $wsFormat $wsFormat
+
+    # Extract the text label (just the ascii no ##: or glyph)
+    local wsLabel=$(echo "${wsArr[$i]}" | jq -Rr '. | match( "(?>\\P{In_Basic_Latin}*)\\K([\\p{ASCII}\\s]+)" ) | .captures[0].string ')
 
     # rename existing workspace with same number but different name
-    local curName=`i3-msg -t get_workspaces | jq -cr --argjson num ${tokens[2]} --arg name ${wsFormat} '.[] | select(.num == $num) | select(.name != $name ) | .name'`
+    local curName=`i3-msg -t get_workspaces | jq -cr --argjson num ${wsNum} --arg name ${wsFormat} '.[] | select(.num == $num) | select(.name != $name ) | .name'`
     if [[ -n $curName ]]; then i3-msg "rename workspace \"${curName}\" to \"${wsFormat}\""; fi
-    
-    sed -i -e "s/${tokens[0]}/\"${wsFormat}\"/g" ${workFile}
+
+    local pat="s/${wsArr[$i]}/${bindSyms}/"
+    sed -i -e "s/{{{${wsLabel}}}}/\"${wsFormat}\"/g" ${workFile}
+    sed -i -e "$(( $i * 2 + $lineNum  + 1 ))s/^.*$/${bindSyms}/" ${workFile}
+
   done
   /usr/bin/cp --backup=t ${workFile} ${configFile}
   rm ${workFile}
