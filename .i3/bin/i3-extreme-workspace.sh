6ccc1a7a065c644d32ebbd381e63401a11677fbb
Templin, Chris <ctemplin@gmail.com>
2021-10-20 21:02:48 -0400

    Style, format, linting: wrapping in function changing shebang shellcheck linting

--- .i3/bin/i3-extreme-workspace.sh
+++ .i3/bin/i3-extreme-workspace.sh
@@ -1,17 +1,21 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 # Swtich to the first or last workspace, optionally moving the focused container
 # too.
-if [ $1 == "focus" ]; then ACTION=""; else ACTION="move"; fi
+main ()
+{
+  if [ "$1" == "focus" ]; then ACTION=""; else ACTION="move"; fi
 
-readarray WS_MIN_MAX <<< $(i3-msg -t get_workspaces | jq -rc '[.[].num] | min, max')
-declare -A INDICIES=( [first]=0 [last]=1 )
-INDEX=${INDICIES["$2"]}
+  readarray WS_MIN_MAX <<< "$(i3-msg -t get_workspaces | jq -rc '[.[].num] | min, max')"
+  local -A INDICIES=( [first]=0 [last]=1 )
+  INDEX=${INDICIES["$2"]}
 
-BAF=""
-if [ $ACTION == "move" ]; then
-  # Command to follow moved container. Simple but not explicit, some configs 
-  # might require a different command.
-  BAF="workspace back_and_forth;"
-fi
-i3-msg "$ACTION workspace number ${WS_MIN_MAX[$INDEX]}; $BAF"
+  local BAF=""
+  if [ $ACTION == "move" ]; then
+    # Command to follow moved container. Simple but not explicit, some configs 
+    # might require a different command.
+    BAF="workspace back_and_forth;"
+  fi
+  i3-msg "$ACTION workspace number ${WS_MIN_MAX[$INDEX]}; $BAF"
+}
+main "$@"
