fe6473a6622a63c4f05bb5b9b0c812f25574ae01
Templin, Chris <ctemplin@gmail.com>
2021-10-24 15:19:30 -0400

    Shrink/grow tiled window to predefined percentages. Auto-determines which dimension should be changed. This is a naive first pass. See comments in file.

---
+++ .i3/bin/i3-cycle-size-ppts.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/bash
+
+main ()
+{
+  # TODO: short-circuit for non-tiling windows
+  source ${HOME}/.bashrc.d/i3-current-workspace.sh
+  # Array of PPT vals to step through
+  local -a PPTS=( 9 12 18 24 32 38 50 62 68 76 82 88 91 )
+  # Get the current PPT
+  CUR_PPT=$(i3-focused-node-ppts)
+  # Get the orientation of the focused node's parent.
+  # TODO: remove assumption that immediate parent has relevant layout/orientation.
+  # TODO: instead walk up node tree until we find appropriate node.
+  local PARENT_ORNTN, DIM
+  PARENT_ORNTN=$(i3-focused-node-parent | jq -r '.orientation')
+  if [[ "$PARENT_ORNTN" == "vertical" ]]; then
+    DIM="height"
+  else
+    DIM="width"
+  fi
+  local PPTS_LEN i TRGT_PPT
+  PPTS_LEN=${#PPTS[@]}
+  if [[ $1 == "grow" ]]; then
+    # Move forward through PPTS
+    for ((i = 0; i <= "$PPTS_LEN"; i++)); do
+      if (( PPTS[i] > CUR_PPT )); then
+        TRGT_PPT="${PPTS[i]}" && break;
+      fi
+    done
+  else
+    # Move backward through PPTS
+    for ((i = $((--PPTS_LEN)); i >= 0; i--)); do
+      if (( PPTS[i] < CUR_PPT )); then
+        TRGT_PPT="${PPTS[i]}" && break;
+      fi
+    done
+  fi
+  if [[ -n "$TRGT_PPT" ]]; then i3-msg resize set "$DIM" "$TRGT_PPT" ppt; fi
+  return 0;
+}
+main "$@"
