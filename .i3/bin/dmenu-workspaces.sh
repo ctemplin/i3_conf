fe2164c2527350f886c703e081391eb1e53799d2
Templin, Chris <ctemplin@gmail.com>
2021-11-03 15:14:15 -0400

    Adding .Xresource colors to dmenu-workspaces pop-up.

--- .i3/bin/dmenu-workspaces.sh
+++ .i3/bin/dmenu-workspaces.sh
@@ -6,7 +6,16 @@ main ()
   get_display_dimensions;
 
   local WS_NAME
-  WS_NAME=$(i3-msg -t get_workspaces | jq -r ' .[] | .name ' | dmenu -l 20 -w 100 -x $(( RES_X/2 - 50 )) -y 200)
+  # Using color values defined in .Xresources via xrdb.
+  # These can be autoloaded with a patched version of dmenu.
+  # Patch at https://tools.suckless.org/dmenu/patches/xresources/
+  WS_NAME=$(i3-msg -t get_workspaces | jq -r ' .[] | .name ' | \
+  dmenu -l 20 -w 120 -x $(( RES_X/2 - 50 )) -y 200 \
+  -fn "NotoSansMono Nerd Font" \
+  -nf "$(xrdb -get dmenu.foreground)" \
+  -nb "$(xrdb -get dmenu.background)" \
+  -sf "$(xrdb -get dmenu.selforeground)" \
+  -sb "$(xrdb -get dmenu.selbackground)" )
   i3-msg workspace "$WS_NAME"
 }
 main "$@"
