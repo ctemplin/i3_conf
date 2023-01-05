44cf91dd9687a5d933ee2d8de2ee2898f2bcc836
Templin, Chris <ctemplin@gmail.com>
2023-01-05 09:34:09 -0500

    Linting fixes

--- .i3/bin/i3-move-container-to-new-workspace.sh
+++ .i3/bin/i3-move-container-to-new-workspace.sh
@@ -2,7 +2,8 @@
 
 main ()
 {
-  export MAX_I3_WS_NUM=`i3-msg -t get_workspaces | jq '[.[].num] | max + 1'`
-  i3-msg move container to workspace $MAX_I3_WS_NUM
+  MAX_I3_WS_NUM=$(i3-msg -t get_workspaces | jq '[.[].num] | max + 1')
+  export MAX_I3_WS_NUM
+  i3-msg move container to workspace "$MAX_I3_WS_NUM"
 }
 main "$@"
