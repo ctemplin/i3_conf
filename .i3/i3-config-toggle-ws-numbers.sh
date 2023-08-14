1f9ccc2fd47802f14eb7b7306539c1ae94bcfa7a
Chris Templin <ctemplin@gmail.com>
2023-08-14 10:19:42 -0400

    Shellcheck fix

--- .i3/i3-config-toggle-ws-numbers.sh
+++ .i3/i3-config-toggle-ws-numbers.sh
@@ -4,7 +4,8 @@ i3-config-toggle-ws-numbers ()
 {
   # Show/hide workspace numbers
   local configFile=~/.i3/config-gen-output/config
-  local curStripNum=$(grep -Po "strip_workspace_numbers\s+\K(\w+)" ${configFile})
+  local curStripNum
+  curStripNum=$(grep -Po "strip_workspace_numbers\s+\K(\w+)" ${configFile})
   declare -A vals=( [yes]="no" [no]="yes" )
   local newStripNum=${vals[$curStripNum]}
   sed -i -e "s/strip_workspace_numbers .*/strip_workspace_numbers ${newStripNum}/" ${configFile}
