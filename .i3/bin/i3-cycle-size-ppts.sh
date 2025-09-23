f51373804470ad5b2a9c9ae00a88b7d7f7137582
Chris Templin <ctemplin@gmail.com>
2025-09-23 11:53:51 -0500

    Adding variation to the ppt cycling Cycles through MAX -> HALF -> MIN -> HALF Each half is approximate and different in order to properly choose MIN or MAX without maintaining state.

--- .i3/bin/i3-cycle-size-ppts.sh
+++ .i3/bin/i3-cycle-size-ppts.sh
@@ -14,6 +14,12 @@ main ()
   
   # Array of PPT vals to step through
   local -a PPTS=( 9 12 18 24 30 38 50 62 70 76 82 88 91 )
+  
+  # For maxMinToggle variation
+  # Two nearly 50% vals. Close enough visually and easy way to determine
+  # if next invocation should grow or shrink.
+  local -a PPTS_PSEUDO_HALVES=( 49 51 )
+
   # Get the current PPT
   CUR_PPT=$(i3-focused-node-ppts)
   # Get the orientation of the focused node's parent.
@@ -35,14 +41,30 @@ main ()
         TRGT_PPT="${PPTS[i]}" && break;
       fi
     done
-  else
+  elif [[ $1 == "shrink" ]]; then
     # Move backward through PPTS
     for ((i = $((--PPTS_LEN)); i >= 0; i--)); do
       if (( PPTS[i] < CUR_PPT )); then
         TRGT_PPT="${PPTS[i]}" && break;
       fi
     done
+  elif [[ $1 == "maxMinToggle" ]]; then
+    # cycle through MAX -> HALF -> MIN -> HALF
+    if (( CUR_PPT == PPTS[-1] )); then
+      TRGT_PPT="${PPTS_PSEUDO_HALVES[-1]}"
+    elif (( CUR_PPT == PPTS_PSEUDO_HALVES[-1] )); then
+      TRGT_PPT="${PPTS[0]}"
+    elif (( CUR_PPT == PPTS_PSEUDO_HALVES[0] )); then
+      TRGT_PPT="${PPTS[-1]}"
+    elif (( CUR_PPT == PPTS[0] )); then
+      TRGT_PPT="${PPTS_PSEUDO_HALVES[0]}"
+    else
+      TRGT_PPT="${PPTS[-1]}"
+    fi
   fi
+
+  # TODO handle default
+
   if [[ -n "$TRGT_PPT" ]]; then i3-msg resize set "$DIM" "$TRGT_PPT" ppt; fi
   return 0;
 }
