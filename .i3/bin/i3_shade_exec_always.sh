0d3881cf1ded579e4cd24f8c5aca54c0029cc9eb
Chris Templin <ctemplin@gmail.com>
2025-10-12 14:48:43 -0500

    Allowing util sh script to exit

--- .i3/bin/i3_shade_exec_always.sh
+++ .i3/bin/i3_shade_exec_always.sh
@@ -4,10 +4,12 @@
 # Starts a nvm-installed i3-shade with nested exempt-callback script
 
 #shellcheck disable=SC1090
-source ~/.bashrc.d/nvm.bash && \
-i3-shade \
+source ~/.bashrc.d/nvm.bash;
+nohup i3-shade \
     --urgent \
     --exemptCallbackExec="  source ~/.bashrc.d/i3-current-workspace.sh && \
                             i3-notify-has-mark _shade_exempt \
                             'i3-shade: Disabled' \
-                            'i3-shade: Enabled'"
+                            'i3-shade: Enabled'" &> /dev/null &
+
+exit
